df8a606d47d2dd4ef3f374a97396700054357d7f
Neil Leonora <nleonora@llu.edu>
2020-07-15 15:35:23 -0700

    added clearance branch

--- lib/generators/devise/views/bulma_templates/bulma_templates_generator.rb
+++ lib/generators/devise/views/bulma_templates/bulma_templates_generator.rb
@@ -4,7 +4,12 @@ module Devise
             source_root File.expand_path('../../../../../../app/views', __FILE__)
 
             def copy_views
-                directory('devise', Rails.root.join('app','views','devise'))
+                # directory('devise', Rails.root.join('app','views','devise'))
+                directory('clearance_mailer', Rails.root.join('app','views', 'clearance_mailer'))
+                directory('passwords', Rails.root.join('app','views', 'passwords'))
+                directory('sessions', Rails.root.join('app','views', 'sessions'))
+                directory('users', Rails.root.join('app','views', 'users'))
+                directory('locales', Rails.root.join('config','locales'))
             end
         end
     end
